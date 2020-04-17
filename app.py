c38cb33aa0831e59a58d64a44a539dc1a9096d6c
Shashankvp000 <shashankvp.000@gmail.com>
2020-04-17 18:49:52 +0530

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -14,12 +14,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exceptio
 app.secret_key = 'corona'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -27,7 +21,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
-
 
 if __name__ == '__main__':
     from db import db
